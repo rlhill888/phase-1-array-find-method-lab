7456a145612b5e6246965b283209586210cb163a
RodneyHill <rlhill888@gmail.com>
2021-12-15 12:27:41 -0500

    commit tree 79bd0d5bbe95202577766c4e89dec9529a80fa6a

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+    //...
+  ]
+superBowlWin = function (record) {
+    array.find(superBowlWin);
+    function superBowlWin() {
+        if (record.result === "W") {
+            return result.year;
+        }
+
+    }
+
+}
